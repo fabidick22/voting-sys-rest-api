53092ae71c05b6a44e42d825761c7db7d7854c0c
fabidick22 <fabidick22@hotmail.es>
2020-03-30 12:10:42 -0500

    Add validations to vote schema

--- models/vote.js
+++ models/vote.js
@@ -3,6 +3,7 @@
 //const mongoose = require('../config/db');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User =  require("../models/user");
 const Area = require('../models/area');
 
 const VoteSchema = new Schema({
@@ -28,5 +29,29 @@ const VoteSchema = new Schema({
         default: Date.now()
     }
 });
+
+VoteSchema.pre('save', function (next) {
+    var self = this;
+    User.find({username : self.user}, function (err, docs) {
+        if (!docs.length){
+            console.log('Username does not exist: ',self.name);
+            next(new Error("Username does not exist!"));
+        }else{
+            next();
+        }
+    });
+}) ;
+
+VoteSchema.pre('save', function (next) {
+    var self = this;
+    Area.find({_id : self.area}, function (err, docs) {
+        if (!docs.length){
+            console.log('ID area does not exist: ',self.name);
+            next(new Error("ID area  does not exist!"));
+        }else{
+            next();
+        }
+    });
+}) ;
 
 module.exports = mongoose.model('Vote', VoteSchema);
